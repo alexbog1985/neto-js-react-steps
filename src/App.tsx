e64a79d934e82db9490c8236195861d644aa2534
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-02-28 08:24:25 +0300

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Form from "./components/Form/Form.tsx";
+import Table from "./components/Table/Table.tsx";
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Form />
+      <Table />
+    </div>
+  )
+}
+
+export default App
