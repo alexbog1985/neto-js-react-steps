fe603215b95501cbe953b9660f0b9ce331ca4773
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-02-28 08:24:40 +0300

    структура

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,9 @@
+function Form() {
+  return (
+    <div className="form-container">
+      Форма
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,9 @@
+function Table() {
+  return (
+    <div className='data-table'>
+      Таблица
+    </div>
+  )
+}
+
+export default Table;
